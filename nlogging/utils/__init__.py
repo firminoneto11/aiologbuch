a7df342983a481765bd2c29bb69d243dc698e127
firminoneto11 <firminoneto11@hotmail.com>
2023-12-19 13:42:54 -0300

    chore: created the utils file

---
+++ nlogging/utils/__init__.py
@@ -0,0 +1,9 @@
+def is_direct_subclass[T: object, I: object](cls_or_instance: type[T] | T, base_cls: I):
+    subclasses = [el.__name__ for el in base_cls.__class__.__subclasses__()]
+
+    # If 'cls_or_instance' is a class, check if it's in subclasses
+    if cls_or_instance.__class__ is type:
+        return cls_or_instance.__name__ in subclasses
+
+    # If 'cls_or_instance' is an instance, check if its class is in subclasses
+    return cls_or_instance.__class__.__name__ in subclasses
