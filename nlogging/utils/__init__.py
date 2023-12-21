928589315fe56774f8cf8d0552ca393be556cd30
firminoneto11 <firminoneto11@hotmail.com>
2023-12-21 12:13:31 -0300

    fix: fixed error on `is_direct_subclass` function

--- nlogging/utils/__init__.py
+++ nlogging/utils/__init__.py
@@ -1,9 +1,9 @@
-def is_direct_subclass[T: object, I: object](cls_or_instance: T, base_cls: I):
-    subclasses = [el.__name__ for el in base_cls.__class__.__subclasses__()]
+def is_direct_subclass[T: object, I: object](value: T, base_cls: I):
+    subclasses = [el.__name__ for el in base_cls.__class__.__subclasses__(base_cls)]
 
-    # If 'cls_or_instance' is a class, check if it's in subclasses
-    if cls_or_instance.__class__ is type:
-        return cls_or_instance.__name__ in subclasses
+    # If 'value' is a class, check if it's name is in the subclasses list
+    if value.__class__ is type:
+        return value.__name__ in subclasses
 
-    # If 'cls_or_instance' is an instance, check if its class is in subclasses
-    return cls_or_instance.__class__.__name__ in subclasses
+    # If 'value' is an instance, check if it's class name is in the subclasses list
+    return value.__class__.__name__ in subclasses
