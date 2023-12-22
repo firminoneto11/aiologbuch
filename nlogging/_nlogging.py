ad7808ff27b405c55701f55a544374c1f10fc061
firminoneto11 <firminoneto11@hotmail.com>
2023-12-22 12:14:02 -0300

    chore: added a type hint and changed docstring

--- nlogging/_nlogging.py
+++ nlogging/_nlogging.py
@@ -7,13 +7,16 @@ from .settings import ROOT_LOGGER_NAME
 
 def get_logger(
     name: str = ROOT_LOGGER_NAME,
-    level: Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"] = "INFO",
+    level: int | Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"] = "INFO",
 ):
     """
-    This function is used to get a logger instance.
+    This function is used to get a logger instance. If you inform the same name, it
+    will always return the same logger.
 
     :param name: The name of the logger. Default is nlogger, which is the root logger.
     :param level: The level of the logger. Default is INFO.
+
+    :return: A NLogger instance.
     """
     manager = LoggerManagerSingleton[NLogger](logger_class=NLogger)
     return manager.get_logger(name=name, level=level)
