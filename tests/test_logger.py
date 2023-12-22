f1d49d32fc31641c02c9720efd13b1fc06c7d3aa
firminoneto11 <firminoneto11@hotmail.com>
2023-12-22 09:56:09 -0300

    test: added test folders

--- tests/test_logger.py
+++
@@ -1,9 +0,0 @@
-from nlogging import get_logger
-
-
-async def test_stuff():
-    log = get_logger()
-    await log.info({"msg": "info"})
-    await log.debug("debug")
-    log = get_logger(level="DEBUG")
-    await log.debug("debug")
