b0a8939cb0425e635aada3668e0d82d98931e645
firminoneto11 <firminoneto11@hotmail.com>
2023-12-31 12:54:01 -0300

    chore: refactored stream handler

--- nlogging/handlers/stream.py
+++ nlogging/handlers/stream.py
@@ -1,11 +1,13 @@
 from asyncio import StreamWriter, get_running_loop
-from typing import TYPE_CHECKING, Optional, TextIO
+from sys import stderr
+from typing import TYPE_CHECKING, Optional
 
 from nlogging.protocols import AIOProtocol
 
 if TYPE_CHECKING:
     from logging import LogRecord
 
+    from nlogging._types import LevelType
     from nlogging.formatters import BaseFormatter
 
 from .base import BaseAsyncHandler, BaseSyncHandler
@@ -14,35 +16,30 @@ from .base import BaseAsyncHandler, BaseSyncHandler
 class AsyncStreamHandler(BaseAsyncHandler):
     _writer: Optional[StreamWriter]
 
-    terminator = "\n"
+    terminator = b"\n"
 
-    def __init__(self, stream: TextIO, level: int | str, formatter: "BaseFormatter"):
+    def __init__(self, level: "LevelType", formatter: "BaseFormatter"):
         super().__init__(level=level, formatter=formatter)
-        self._closed = True
+        self.closed = True
         self._writer = None
-        self._stream = stream
-
-    @property
-    def closed(self):
-        return self._closed
 
     @property
     def writer(self):
         return self._writer
 
-    @property
-    def stream(self):
-        return self._stream
+    @writer.setter
+    def writer(self, value: Optional[StreamWriter]):
+        if self.is_open:
+            raise RuntimeError("Cannot change writer on open handler")
 
-    @stream.setter
-    def stream(self, value: TextIO):
-        if not self.closed:
-            raise ValueError("Cannot change stream on open handler")
-
-        if value is self.stream:
+        if self.writer is value:
             return
 
-        self._stream = value
+        self._writer = value
+
+    @property
+    def is_open(self):
+        return bool(self.writer) and (not self.closed)
 
     async def emit(self, record: "LogRecord"):
         try:
@@ -51,51 +48,41 @@ class AsyncStreamHandler(BaseAsyncHandler):
         except:  # noqa
             await self.handle_error(record)
 
-    async def write_and_flush(self, msg: str):
-        writer = await self._init_writer()
-        writer.write(msg.encode())
-        await writer.drain()
+    async def write_and_flush(self, msg: bytes):
+        if not self.is_open:
+            await self._init_writer()
+
+        if not self.writer:
+            # NOTE: This is not likely to happen, but you never know...
+            raise RuntimeError("Writer was not initialized")
+
+        async with self.lock:
+            self.writer.write(msg)
+            await self.writer.drain()
 
     async def close(self):
-        if (self.writer) and (not self.closed):
-            await self.acquire()
-            try:
+        if self.is_open:
+            async with self.lock:
                 await self.writer.drain()
                 self.writer.close()
-                await self.writer.wait_closed()
-                self._closed = True
-                self._writer = None
-            finally:
-                self.release()
+                self.writer = None
+                self.closed = True
 
     async def _init_writer(self):
-        if (self.writer) and (not self.closed):
-            return self.writer
-
         loop = get_running_loop()
-
-        await self.acquire()
-        try:
-            transport, protocol = await loop.connect_write_pipe(
-                AIOProtocol, self.stream
-            )
-            self._writer = StreamWriter(
+        async with self.lock:
+            transport, protocol = await loop.connect_write_pipe(AIOProtocol, stderr)
+            self.writer = StreamWriter(
                 transport=transport, protocol=protocol, reader=None, loop=loop
             )
-            self._closed = False
-            return self._writer
-        finally:
-            self.release()
+            self.closed = False
 
 
 class SyncStreamHandler(BaseSyncHandler):
-    _writer: Optional[StreamWriter]
+    terminator = b"\n"
 
-    terminator = "\n"
-
-    def __init__(self, stream: TextIO, level: int | str, formatter: "BaseFormatter"):
+    def __init__(self, level: "LevelType", formatter: "BaseFormatter"):
         super().__init__(level=level, formatter=formatter)
-        self.stream = stream
 
     def emit(self, record: "LogRecord"):
         try:
@@ -104,11 +91,7 @@ class SyncStreamHandler(BaseSyncHandler):
         except:  # noqa
             self.handle_error(record)
 
-    def write_and_flush(self, msg: str):
-        self.stream.write(msg)
-        self.acquire()
-        try:
-            if self.stream and hasattr(self.stream, "flush"):
-                self.stream.flush()
-        finally:
-            self.release()
+    def write_and_flush(self, msg: bytes):
+        with self.lock:
+            stderr.write(msg.decode())
+            stderr.flush()
