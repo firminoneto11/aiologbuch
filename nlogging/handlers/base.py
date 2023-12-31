e07d5203dd117e1f0a6b7098a105f8252f322c04
firminoneto11 <firminoneto11@hotmail.com>
2023-12-31 12:53:28 -0300

    chore: using a global lock instead of instance scoped

--- nlogging/handlers/base.py
+++ nlogging/handlers/base.py
@@ -1,8 +1,10 @@
-from asyncio import Lock, to_thread
 from functools import lru_cache
 from logging import Handler
 from threading import RLock
 from typing import TYPE_CHECKING, Optional
+
+from anyio import Lock
+from anyio.to_thread import run_sync
 
 from nlogging.filters import Filterer
 from nlogging.formatters import BaseFormatter
@@ -13,6 +15,8 @@ from nlogging.utils import is_direct_subclass
 if TYPE_CHECKING:
     from logging import LogRecord
 
+    from nlogging._types import LevelType
+
 
 @lru_cache(maxsize=1)
 def _handler_id_generator():
@@ -22,14 +26,17 @@ def _handler_id_generator():
         i += 1
 
 
+_async_lock = Lock()
+_sync_lock = RLock()
+
+
 class BaseAsyncHandler(Filterer):
     _formatter: Optional[BaseFormatter]
 
-    def __init__(self, level: int | str, formatter: BaseFormatter):
+    def __init__(self, level: "LevelType", formatter: BaseFormatter):
         super().__init__()
         self.level = level
         self.formatter = formatter
-        self._lock = Lock()
         self._id = next(_handler_id_generator())
 
     @property
@@ -37,7 +44,7 @@ class BaseAsyncHandler(Filterer):
         return self._level
 
     @level.setter
-    def level(self, value: int | str):
+    def level(self, value: "LevelType"):
         self._level = check_level(value)
 
     @property
@@ -52,7 +59,7 @@ class BaseAsyncHandler(Filterer):
 
     @property
     def lock(self):
-        return self._lock
+        return _async_lock
 
     @property
     def id(self):
@@ -60,9 +67,6 @@ class BaseAsyncHandler(Filterer):
 
     async def emit(self, record: "LogRecord") -> None:
         raise NotImplementedError("emit must be implemented by Handler subclasses")
-
-    # async def flush(self) -> None:
-    #     raise NotImplementedError("flush must be implemented by Handler subclasses")
 
     async def close(self) -> None:
         raise NotImplementedError("close must be implemented by Handler subclasses")
@@ -79,23 +83,17 @@ class BaseAsyncHandler(Filterer):
 
     async def handle_error(self, record: "LogRecord"):
         if RAISE_EXCEPTIONS:
-            await to_thread(Handler.handleError, None, record)
-
-    async def acquire(self):
-        await self.lock.acquire()
-
-    def release(self):
-        self.lock.release()
+            async with self.lock:
+                await run_sync(Handler.handleError, None, record)
 
 
 class BaseSyncHandler(Filterer):
     _formatter: Optional[BaseFormatter]
 
-    def __init__(self, level: int | str, formatter: BaseFormatter):
+    def __init__(self, level: "LevelType", formatter: BaseFormatter):
         super().__init__()
         self.level = level
         self.formatter = formatter
-        self._lock = RLock()
         self._id = next(_handler_id_generator())
 
     @property
@@ -103,7 +101,7 @@ class BaseSyncHandler(Filterer):
         return self._level
 
     @level.setter
-    def level(self, value: int | str):
+    def level(self, value: "LevelType"):
         self._level = check_level(value)
 
     @property
@@ -118,7 +116,7 @@ class BaseSyncHandler(Filterer):
 
     @property
     def lock(self):
-        return self._lock
+        return _sync_lock
 
     @property
     def id(self):
@@ -140,9 +138,3 @@ class BaseSyncHandler(Filterer):
     def handle_error(self, record: "LogRecord"):
         if RAISE_EXCEPTIONS:
             Handler.handleError(None, record)
-
-    def acquire(self):
-        self.lock.acquire()
-
-    def release(self):
-        self.lock.release()
