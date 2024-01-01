d3443806860dc57c7168d22aaba389a01fec3f52
firminoneto11 <firminoneto11@hotmail.com>
2024-01-01 13:48:34 -0300

    chore: implemented `_get_close_waiter` for the `AIOProtocol class`

--- nlogging/protocols/__init__.py
+++ nlogging/protocols/__init__.py
@@ -1,6 +1,15 @@
-from asyncio import Protocol
+from asyncio import sleep
+from asyncio.protocols import Protocol
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from asyncio import StreamWriter
 
 
 class AIOProtocol(Protocol):
     async def _drain_helper(self):
         ...
+
+    async def _get_close_waiter(self, transport: "StreamWriter"):
+        while transport.transport._pipe is not None:
+            await sleep(0)
