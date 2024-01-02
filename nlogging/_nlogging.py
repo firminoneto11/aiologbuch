a08d5d8fde51b096c8bc2764ac63eb541668c6e3
firminoneto11 <firminoneto11@hotmail.com>
2024-01-02 09:27:46 -0300

    chore: creating a new async file handler should the user pass a filename into the `get_logger`func

--- nlogging/_nlogging.py
+++ nlogging/_nlogging.py
@@ -1,13 +1,14 @@
-from typing import Literal
-
-from .loggers import NLogger
-from .manager import AsyncLoggerManagerSingleton
+from ._types import LevelType
+from .formatters import JsonFormatter
+from .handlers import AsyncFileHandler
+from .loggers import AsyncLoggerManagerSingleton, NLogger
 from .settings import ROOT_LOGGER_NAME
 
 
 def get_logger(
     name: str = ROOT_LOGGER_NAME,
-    level: int | Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"] = "INFO",
+    level: LevelType = "INFO",
+    filename: str = "",
 ):
     """
     This function is used to get a logger instance. If you inform the same name, it
@@ -15,8 +16,16 @@ def get_logger(
 
     :param name: The name of the logger. Default is nlogger, which is the root logger.
     :param level: The level of the logger. Default is INFO.
+    :param filename: The filename to write the logs.
 
     :return: A NLogger instance.
     """
     manager = AsyncLoggerManagerSingleton[NLogger](logger_class=NLogger)
-    return manager.get_logger(name=name, level=level)
+    logger = manager.get_logger(name=name, level=level)
+
+    if filename:
+        logger.add_handler(
+            AsyncFileHandler(filename=filename, level=level, formatter=JsonFormatter())
+        )
+
+    return logger
