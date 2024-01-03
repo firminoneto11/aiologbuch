8f30ed556d7c9cf21ec51c7edb7a39759dd075ff
firminoneto11 <firminoneto11@hotmail.com>
2024-01-03 12:47:51 -0300

    chore: reduced couple and simplified filter process

--- nlogging/handlers/base.py
+++ nlogging/handlers/base.py
@@ -1,20 +1,23 @@
 from functools import lru_cache
 from logging import Handler
-from typing import TYPE_CHECKING, Optional
+from typing import TYPE_CHECKING
 
 from anyio import Lock
 from anyio.to_thread import run_sync
 
-from nlogging.filters import Filterer
-from nlogging.formatters import BaseFormatter
+from nlogging.filters import Filter
 from nlogging.levels import check_level
 from nlogging.settings import RAISE_EXCEPTIONS
-from nlogging.utils import is_direct_subclass
 
 if TYPE_CHECKING:
     from logging import LogRecord
+    from typing import Protocol
 
     from nlogging._types import LevelType
+
+    class FormatterProtocol(Protocol):
+        def format(self, record: "LogRecord") -> bytes:
+            ...
 
 
 @lru_cache(maxsize=1)
@@ -30,12 +33,10 @@ def get_stderr_lock():
     return Lock()
 
 
-class BaseAsyncHandler(Filterer):
-    _formatter: Optional[BaseFormatter]
+class BaseAsyncHandler:
     terminator = b"\n"
 
-    def __init__(self, level: "LevelType", formatter: BaseFormatter):
-        super().__init__()
+    def __init__(self, level: "LevelType", formatter: "FormatterProtocol"):
         self._id = next(_handler_id_generator())
         self.formatter = formatter
         self.level = level
@@ -45,22 +46,13 @@ class BaseAsyncHandler(Filterer):
         return self._id
 
     @property
-    def formatter(self):
-        return self._formatter
-
-    @formatter.setter
-    def formatter(self, value: BaseFormatter):
-        if not is_direct_subclass(value=value, base_cls=BaseFormatter):
-            raise TypeError("'formatter' must be a subclass of BaseFormatter")
-        self._formatter = value
-
-    @property
     def level(self):
         return self._level
 
     @level.setter
     def level(self, value: "LevelType"):
         self._level = check_level(value)
+        self.filter = Filter(level=self.level)
 
     async def emit(self, record: "LogRecord"):
         try:
@@ -78,14 +70,11 @@ class BaseAsyncHandler(Filterer):
         raise NotImplementedError("'close' must be implemented by Handler subclasses")
 
     def format(self, record: "LogRecord"):
-        if not self.formatter:
-            raise TypeError("No formatter found")
         return self.formatter.format(record)
 
     async def handle(self, record: "LogRecord"):
-        if rv := self.filter(record):
+        if self.filter.filter(record):
             await self.emit(record)
-        return rv
 
     async def handle_error(self, record: "LogRecord"):
         if RAISE_EXCEPTIONS:
