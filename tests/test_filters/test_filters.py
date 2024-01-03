28ca39058b7be419017e6ced690ba4297a543fb9
firminoneto11 <firminoneto11@hotmail.com>
2024-01-03 12:45:19 -0300

    test: removed old filterer tests

--- tests/test_filters/test_filters.py
+++ tests/test_filters/test_filters.py
@@ -3,7 +3,6 @@ from unittest.mock import MagicMock
 from pytest import mark
 
 from nlogging.filters import Filter
-from nlogging.filters.filter import _filter_id_generator
 
 
 @mark.unit
@@ -21,16 +20,5 @@ def test_filter(filter_level: int, record_level: int, expected: bool):
     _filter = Filter(level=filter_level)
     record = MagicMock(levelno=record_level)
 
-    assert _filter.id == next(_filter_id_generator()) - 1
     assert _filter.level == filter_level
     assert _filter.filter(record) == expected
-
-
-@mark.unit
-def test_filter_id_generator():
-    latest_id = next(_filter_id_generator())
-
-    assert hex(id(_filter_id_generator())) == hex(id(_filter_id_generator()))
-    assert next(_filter_id_generator()) == latest_id + 1
-    assert next(_filter_id_generator()) == latest_id + 2
-    assert next(_filter_id_generator()) == latest_id + 3
