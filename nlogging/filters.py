64e9b61e0a5bf2b396c11b98d231f9264e9d0668
firminoneto11 <firminoneto11@hotmail.com>
2024-01-04 10:46:32 -0300

    chore: simplified filter

--- nlogging/filters.py
+++ nlogging/filters.py
@@ -1,29 +1,17 @@
 from typing import TYPE_CHECKING
 
-from nlogging.levels import check_level
-
 if TYPE_CHECKING:
     from logging import LogRecord
 
-    from nlogging._types import LevelType
-
 
 class Filter:
-    def __init__(self, level: "LevelType"):
-        self._level = check_level(level)
-
-    @property
-    def level(self):
-        return self._level
-
-    @level.setter
-    def level(self, value: "LevelType"):
-        self._level = check_level(value)
+    def __init__(self, level: int):
+        self._level = level
 
     def filter(self, record: "LogRecord"):
-        return record.levelno >= self.level
+        return record.levelno >= self._level
 
 
 class ExclusiveFilter(Filter):
     def filter(self, record: "LogRecord"):
-        return record.levelno == self.level
+        return record.levelno == self._level
