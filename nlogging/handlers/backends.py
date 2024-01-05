1c220c5a5bf078742aa2a17fb4912a34dfcf79a4
firminoneto11 <firminoneto11@hotmail.com>
2024-01-05 09:57:44 -0300

    chore: created a backends module this module will hold the underlying backend implementations concerning file handling. aiofile or threads for now

---
+++ nlogging/handlers/backends.py
@@ -0,0 +1,75 @@
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, Literal
+
+from aiofile import async_open
+from anyio import Lock
+from anyio.streams.file import FileWriteStream
+
+if TYPE_CHECKING:
+    from aiofile import BinaryFileWrapper
+
+    from nlogging._types import BackendProtocol
+
+
+def get_backend(name: Literal["thread", "aiofile"]) -> "BackendProtocol":
+    match name.lower().strip():
+        case "thread":
+            return ThreadBackend
+        case "aiofile":
+            return AIOBackend
+        case _:
+            raise ValueError("Invalid backend name")
+
+
+@dataclass
+class AIOBackend:
+    filename: str
+    _lock: Lock = None
+    _file_stream: "BinaryFileWrapper | None" = None
+
+    def __post_init__(self):
+        self._lock = Lock()
+
+    async def init(self):
+        async with self._lock:
+            if not self._file_stream:
+                self._file_stream = await async_open(self.filename, mode="a+b")
+
+    async def send(self, msg: bytes):
+        async with self._lock:
+            if not self._file_stream:
+                return
+            await self._file_stream.write(msg)
+
+    async def close(self):
+        async with self._lock:
+            if self._file_stream:
+                await self._file_stream.close()
+                self._file_stream = None
+
+
+@dataclass
+class ThreadBackend:
+    filename: str
+    _lock: Lock = None
+    _file_stream: "FileWriteStream | None" = None
+
+    def __post_init__(self):
+        self._lock = Lock()
+
+    async def init(self):
+        async with self._lock:
+            if not self._file_stream:
+                self._file_stream = await FileWriteStream.from_path(self.filename, True)
+
+    async def send(self, msg: bytes):
+        async with self._lock:
+            if not self._file_stream:
+                return
+            await self._file_stream.send(msg)
+
+    async def close(self):
+        async with self._lock:
+            if self._file_stream:
+                await self._file_stream.aclose()
+                self._file_stream = None
