4bb5734e4af8f716340d632d0790259eaeaf905b
firminoneto11 <firminoneto11@hotmail.com>
2024-01-06 14:05:55 -0300

    chore: moved the resource methods outside of the manager's lock scope

--- nlogging/handlers/file.py
+++ nlogging/handlers/file.py
@@ -49,21 +49,19 @@ class _StreamResource:
 
 @dataclass
 class _ResourceManager:
-    _lock = Lock()
     _map: dict[str, "MapType"] = None
+    _lock: Lock = None
 
-    @property
-    def lock(self):
-        return self._lock
+    def __post_init__(self):
+        self._lock = Lock()
+        self._map = {}
 
     @property
     def map(self):
-        if not self._map:
-            self._map = {}
         return self._map
 
     async def request_resource(self, filename: str):
-        async with self.lock:
+        async with self._lock:
             if content := self.map.get(filename):
                 resource = content["resource"]
                 content["reference_count"] += 1
@@ -71,26 +69,26 @@ class _ResourceManager:
                 resource = _StreamResource(filename=filename)
                 self.map[filename] = {"resource": resource, "reference_count": 1}
 
-            await resource.init_stream()
+        await resource.init_stream()
 
     async def send_message(self, filename: str, msg: bytes):
-        async with self.lock:
+        async with self._lock:
             if not (content := self.map.get(filename)):
                 raise RuntimeError("Resource is not initialized")
 
-            await content["resource"].send(msg)
+        await content["resource"].send(msg)
 
     async def close_resource(self, filename: str):
         resource = None
 
-        async with self.lock:
+        async with self._lock:
             if content := self.map.get(filename):
                 content["reference_count"] -= 1
                 if content["reference_count"] <= 0:
                     resource = self.map.pop(filename)["resource"]
 
-            if resource:
-                await resource.close()
+        if resource:
+            await resource.close()
 
 
 class AsyncFileHandler(BaseAsyncHandler):
