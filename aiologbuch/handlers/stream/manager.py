b07f0221a51e461afd357a959648cb7089ac4560
firminoneto11 <firminoneto11@hotmail.com>
2024-06-02 21:07:32 -0300

    fix: fixed lock access in sync manager

--- aiologbuch/handlers/stream/manager.py
+++ aiologbuch/handlers/stream/manager.py
@@ -1,6 +1,7 @@
 import sys
 from asyncio import StreamWriter, get_running_loop, sleep
 from asyncio.protocols import Protocol
+from contextlib import contextmanager
 from dataclasses import dataclass
 from typing import Optional, TextIO
 
@@ -15,6 +16,15 @@ class _AIOProto(Protocol):
     async def _get_close_waiter(self, transport: StreamWriter):
         while transport.transport._pipe is not None:
             await sleep(0)  # Skips one event loop iteration
+
+
+@contextmanager
+def _syncify_lock():
+    syncify(STDERR_LOCK.acquire)()
+    try:
+        yield
+    finally:
+        syncify(STDERR_LOCK.release)()
 
 
 @dataclass
@@ -45,12 +55,11 @@ class _ResourceManager:
             await self._writer.drain()
 
     def send_message(self, msg: bytes):
-        syncify(STDERR_LOCK.acquire)()
-        try:
+        with _syncify_lock():
+            if self.closed:
+                raise RuntimeError("Writer was closed")
             self.stream.write(msg)
             self.stream.flush()
-        finally:
-            syncify(STDERR_LOCK.release)()
 
     # NOTE: Is it wise to close the sys.stderr?
 
@@ -67,13 +76,13 @@ class _ResourceManager:
             self._writer, self._closed = None, True
 
     def close(self):
-        syncify(STDERR_LOCK.acquire)()
-        try:
+        with _syncify_lock():
+            if self.closed:
+                return
             self.stream.write("Closing stderr...")
             self.stream.flush()
             self.stream.close()
-        finally:
-            syncify(STDERR_LOCK.release)()
+            self._closed = True
 
 
 resource_manager = _ResourceManager(stream=sys.stderr)
