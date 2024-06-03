0da218051651c9c7e4c9a708ed064365319d1ecd
firminoneto11 <firminoneto11@hotmail.com>
2024-06-02 21:01:45 -0300

    chore: refactored the async logger

--- aiologbuch/loggers/base.py
+++ aiologbuch/loggers/base.py
@@ -1,59 +1,30 @@
-from abc import ABC, abstractmethod
+from inspect import stack
+from logging import LogRecord
+from traceback import format_exception
 from typing import TYPE_CHECKING, Optional
 
 if TYPE_CHECKING:
-    from aiologbuch._types import (
-        AsyncHandlerProtocol,
-        CallerInfo,
-        FilterProtocol,
-        LogRecordProtocol,
-        MessageType,
-    )
+    from aiologbuch.types import CallerInfo, LoggerKind, LogRecordProtocol, MessageType
 
 
-class BaseAsyncLogger(ABC):
-    _handlers: dict[int, "AsyncHandlerProtocol"]
+class BaseLogger[HandlerProtocol]:
+    _enabled = True
+    _handlers: set[HandlerProtocol]
+    kind: "LoggerKind"
+    name: str
 
-    @abstractmethod
-    def __init__(self, name: str, filter: "FilterProtocol") -> None:
-        ...
+    def __init__(self, name: str):
+        self.name = name
+        self._handlers = set()
 
-    @property
-    @abstractmethod
-    def level(self) -> int:
-        ...
-
-    @abstractmethod
-    def _do_filtering(self, level: int) -> bool:
-        ...
-
-    @abstractmethod
-    async def debug(self, msg: "MessageType") -> None:
-        ...
-
-    @abstractmethod
-    async def info(self, msg: "MessageType") -> None:
-        ...
-
-    @abstractmethod
-    async def warning(self, msg: "MessageType") -> None:
-        ...
-
-    @abstractmethod
-    async def error(
-        self, msg: "MessageType", exc: Optional[BaseException] = None
-    ) -> None:
-        ...
-
-    @abstractmethod
-    async def critical(self, msg: "MessageType") -> None:
-        ...
-
-    @abstractmethod
     def _find_caller(self) -> "CallerInfo":
-        ...
+        frame = stack()[3]  # 3 frames up from this one is the original caller
+        return {
+            "filename": frame.filename,
+            "function_name": frame.function,
+            "line_number": frame.lineno,
+        }
 
-    @abstractmethod
     def _make_record(
         self,
         name: str,
@@ -64,38 +35,20 @@ class BaseAsyncLogger(ABC):
         line_number: int,
         exc_info: Optional[BaseException] = None,
     ) -> "LogRecordProtocol":
-        ...
+        if exc_info:
+            info = (type(exc_info), exc_info, exc_info.__traceback__)
+            text = "".join(format_exception(exc_info, limit=None, chain=True))
+        else:
+            info, text = None, None
 
-    @abstractmethod
-    async def _log(
-        self, level: int, msg: "MessageType", exc_info: Optional[BaseException] = None
-    ) -> None:
-        ...
+        record = LogRecord(
+            name, level, filename, line_number, msg, None, function_name, info
+        )
 
-    @abstractmethod
-    async def _handle(self, record: "LogRecordProtocol") -> None:
-        ...
+        if text:
+            record.exc_text = text
 
-    @abstractmethod
-    def _add_handler(self, handler: "AsyncHandlerProtocol") -> None:
-        ...
+        return record
 
-    @abstractmethod
-    def _remove_handler(self, handler: "AsyncHandlerProtocol") -> None:
-        ...
-
-    @abstractmethod
-    def _has_handlers(self) -> bool:
-        ...
-
-    @abstractmethod
-    async def _call_handlers(self, record: "LogRecordProtocol") -> None:
-        ...
-
-    @abstractmethod
-    def _is_enabled_for(self, level: int) -> bool:
-        ...
-
-    @abstractmethod
-    async def _disable(self) -> None:
-        ...
+    def _add_handler(self, handler: HandlerProtocol):
+        self._handlers.add(handler)
