e6a381f9ff1d4433dbf6d49cb7171e21f5ae6e11
firminoneto11 <firminoneto11@hotmail.com>
2024-06-07 19:29:16 -0300

    chore: simplified filters

--- aiologbuch/filters.py
+++ aiologbuch/filters.py
@@ -1,9 +1,3 @@
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from aiologbuch.types import LogRecordProtocol
-
-
 class Filter:
     def __init__(self, level: int):
         self._level = level
@@ -12,14 +6,10 @@ class Filter:
     def level(self):
         return self._level
 
-    def filter(self, record: "LogRecordProtocol | int"):
-        if isinstance(record, int):
-            return record >= self.level
-        return record.levelno >= self.level
+    def filter(self, level: int):
+        return level >= self.level
 
 
 class ExclusiveFilter(Filter):
-    def filter(self, record: "LogRecordProtocol | int"):
-        if isinstance(record, int):
-            return record == self.level
-        return record.levelno == self.level
+    def filter(self, level: int):
+        return level == self.level
