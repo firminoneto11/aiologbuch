e43c75a4758f4bbbcb03d055941325728a350620
firminoneto11 <firminoneto11@hotmail.com>
2024-06-15 10:25:23 -0300

    chore: added type hints to the stream backend constant

--- aiologbuch/shared/conf.py
+++ aiologbuch/shared/conf.py
@@ -1,5 +1,6 @@
 from asyncio import Lock
 from os import getenv
+from typing import Literal
 
 
 def _parse_bool(value: str):
@@ -13,4 +14,6 @@ GLOBAL_STDERR_LOCK = Lock()
 
 RAISE_EXCEPTIONS = _parse_bool(getenv("AIOLOGBUCH_RAISE_EXCEPTIONS", "true"))
 
-STREAM_BACKEND = getenv("AIOLOGBUCH_STREAM_BACKEND", "thread").lower().strip()
+STREAM_BACKEND: Literal["thread", "aiofile"] = (
+    getenv("AIOLOGBUCH_STREAM_BACKEND", "thread").lower().strip()
+)
