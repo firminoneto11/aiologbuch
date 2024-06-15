a514de99c6d957bf6f7901394af9222cb7e46c8e
firminoneto11 <firminoneto11@hotmail.com>
2024-06-15 10:24:58 -0300

    chore: correctly initializing the file handler classes

--- aiologbuch/handlers/__init__.py
+++ aiologbuch/handlers/__init__.py
@@ -3,30 +3,35 @@ from typing import TYPE_CHECKING as _TypeChecking
 from .base import BaseAsyncHandler as _BaseAsync
 from .base import BaseSyncHandler as _BaseSync
 from .file import AsyncFileMixin as _AsyncFileMixin
+from .file import SyncFileMixin as _SyncFileMixin
 from .stderr import AsyncStderrMixin as _AsyncStderrMixin
 from .stderr import SyncStderrMixin as _SyncStderrMixin
 
 if _TypeChecking:
-    from aiologbuch.shared.types import FilterProtocol, FormatterProtocol
+    from aiologbuch.shared.types import FormatterProtocol
 
 
 class AsyncStderrHandler(_BaseAsync, _AsyncStderrMixin):
     ...
 
 
+class AsyncFileHandler(_BaseAsync, _AsyncFileMixin):
+    def __init__(self, filename: str, formatter: "FormatterProtocol"):
+        if not filename:
+            raise ValueError("'filename' cannot be empty")
+
+        super(_BaseAsync, self).__init__(formatter=formatter)
+        self._filename = filename
+
+
 class SyncStderrHandler(_BaseSync, _SyncStderrMixin):
     ...
 
 
-class AsyncFileHandler(_BaseAsync, _AsyncFileMixin):
-    def __init__(
-        self, filename: str, filter: "FilterProtocol", formatter: "FormatterProtocol"
-    ):
+class SyncFileHandler(_BaseSync, _SyncFileMixin):
+    def __init__(self, filename: str, formatter: "FormatterProtocol"):
         if not filename:
             raise ValueError("'filename' cannot be empty")
-        super(_BaseAsync, self).__init__(filter=filter, formatter=formatter)
+
+        super(_BaseSync, self).__init__(formatter=formatter)
         self._filename = filename
-
-
-# class SyncFileHandler(_BaseSync, _SyncFileMixin):
-#     ...
